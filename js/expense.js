d16517e2eff1bb7b94089d200af30be195d76d4d
Maverick Madison <maverickgm9@gmail.com>
2025-05-31 10:41:45 -0700

    Add functionality skeletons to js file

--- js/expense.js
+++ js/expense.js
@@ -3,4 +3,38 @@ let totalAmount = 0;
 
 const form = document.getElementById('expenseForm');
 const expensesList = document.getElementById('expensesList');
-const totalAmountSpent = document
+const totalAmountSpent = document.getElementById('totalAmount');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const description = document.getElementById('description').value;
+    const amount = parseFloat(document.getElementById('amount').value);
+    const category = document.getElementById('category').value;
+
+    // create new object for the new expense
+    const newExpense = {
+        id: Date.now(),
+        description: description,
+        amount: amount,
+        category: category,
+        date: new Date().toLocaleDateString()
+    }
+    
+    expenses.push(newExpense);
+
+
+});
+
+function updateExpensesList() {
+
+}
+
+function updateTotal() {
+
+}
+
+function deleteExpense(id) {
+
+}
+
