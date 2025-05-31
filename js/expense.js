1fa3cf49d1dafbae99b2a66973fbab13e0e2a9ad
Maverick Madison <maverickgm9@gmail.com>
2025-05-31 10:35:30 -0700

    add stylings to total section and for delete btn

---
+++ js/expense.js
@@ -0,0 +1,6 @@
+let expenses = [];
+let totalAmount = 0;
+
+const form = document.getElementById('expenseForm');
+const expensesList = document.getElementById('expensesList');
+const totalAmountSpent = document
